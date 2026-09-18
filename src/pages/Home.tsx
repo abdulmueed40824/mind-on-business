1f55d4e733fb1876a706aee482b4f4ba1343b55b
Taimoor Khan <40888@contoso.local>
2026-09-18 05:39:05 +0500

    init changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,6 @@ const Home: React.FC = () => {
           <Hero />
           <About />
           <Collection />
-          <FeaturedProducts />
           <Ethos />
           <Lookbook />
           <Differentiators />
